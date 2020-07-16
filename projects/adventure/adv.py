71984ad81424a2c7305bfa424a713dd483cf8cf6
Jon Haase <haase1020@gmail.com>
2020-07-16 17:03:03 -0400

    update adv.py file and initial tests passing

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -5,16 +5,18 @@ from world import World
 import random
 from ast import literal_eval
 
+from util import Stack
+
 # Load world
 world = World()
 
 # 500 rooms
 # You may uncomment the smaller graphs for development and testing purposes.
-map_file = "maps/test_line.txt"
+# map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-# map_file = "maps/main_maze.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph = literal_eval(open(map_file, "r").read())
@@ -30,7 +32,50 @@ player = Player(world.starting_room)
 traversal_path = []
 
 
-# TRAVERSAL TEST
+def reversal_path(direction):
+    if direction == "n":
+        return "s"
+    elif direction == "s":
+        return "n"
+    elif direction == "e":
+        return "w"
+    elif direction == "w":
+        return "e"
+
+
+def traverse_map(starting_room):
+    stack = Stack()  # DF
+    visited = set()
+    starting_room = player.current_room
+    # itereate through exits
+    while len(visited) < len(world.rooms):
+        path = []
+        for room_exit in player.current_room.get_exits():
+            if room_exit is not None:
+                if player.current_room.get_room_in_direction(room_exit) not in visited:
+                    path.append(room_exit)
+        # if exit has not been visited, travel to that exit
+        # player.travel(room_exit)
+        visited.add(player.current_room)
+
+        if len(path) != 0:
+            move = random.randint(0, len(path) - 1)
+            stack.push(path[move])
+            player.travel(path[move])
+            traversal_path.append(path[move])
+
+        else:
+            last = stack.pop()
+            player.travel(reversal_path(last))
+            traversal_path.append(reversal_path(last))
+
+
+# check to see if at a dead end
+# if at a dead end move in reverse until get to a room that is not visited
+# figure out how to update traversal path
+traverse_map(0)
+
+# TRAVERSAL TEST - DO NOT CHANGE
 visited_rooms = set()
 player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
